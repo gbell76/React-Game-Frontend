bac84f68d56f3df41c02118300e36177a74b892b
Gina Sciacchitano <gbell76767@gmail.com>
2024-05-09 22:42:01 -0400

    added preferences and other tweaks

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # Bot Battle
 
 ## Description
-Bot Battle is a simple turn based pvp game featuring robots with stats each player can customize.
+Bot Battle is a simple turn based pvp game built in react where robots with customizable stats battle each other to be the last one standing.
 
 ## Instillation
 To run locally, download both this repository and the React-Game-Backend repository and run the server on that reposity while running dev on this one.
 
 ## Credit
 Robot icons downloaded from FreePik
+Hex shape code from https://jtauber.github.io/articles/css-hexagon.html
 
 ## Usage
 Create an account if you do not have one or log into one if you do. Navigate to the Challenge tab for a 1v1 game, FFA for a 3 player free for all, or Mayhem for a 4 person free for all game. Use the + and - buttons to adjust your stats until you have no more stat points then allow the other players to do the same for their stats. When you see a hex grid, it's time to play! Each player gets 2 actions per turn. Those actions can be to either move up to your move stat in hexes or attack another robot that is within your range. The last player standing wins.
@@ -16,3 +17,5 @@ Create an account if you do not have one or log into one if you do. Navigate to 
 
 
 ## Screenshots
+![alt text](<src/assets/Screenshot 2024-05-09 000050.png>)
+![alt text](<src/assets/Screenshot 2024-05-08 235429.png>)
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,87 @@ import Auth from '../utils/auth'
 
 const Home = () => {
 
+    const [user, setUser] = useState([])
+    const [edit, setEdit] = useState(false)
+    const [rows, setRows] = useState()
+    const [columns, setColumns] = useState()
+    const [statPoints, setStatPoints] = useState()
+    const [actionPoints, setActionPoints] = useState()
+
+    const toggle = () => {
+        setRows(user[0].preferences.rows)
+        setColumns(user[0].preferences.columns)
+        setStatPoints(user[0].preferences.statPoints)
+        setActionPoints(user[0].preferences.actionPoints)
+        setEdit(!edit)
+    }
+
+    const submit = async () => {
+        const savePreferences = await fetch(`http://localhost:5000/user/${Auth.getProfile().data._id}`, {
+            method: 'PUT',
+            body: JSON.stringify({ preferences: { rows: rows, columns: columns, statPoints: statPoints, actionPoints: actionPoints } }),
+            headers: { 'Content-Type': 'application/json' }
+        })
+        const response = await fetch(`http://localhost:5000/user/${Auth.getProfile().data._id}`)
+        const data = await response.json()
+        setUser(data)
+        localStorage.setItem('rows', data[0].preferences.rows)
+        localStorage.setItem('columns', data[0].preferences.columns)
+        localStorage.setItem('actionPoints', data[0].preferences.actionPoints)
+        localStorage.setItem('statPoints', data[0].preferences.statPoints)
+        toggle()
+    }
+
     useEffect(() => {
         Auth.checkLoggedIn()
+        fetch(`http://localhost:5000/user/${Auth.getProfile().data._id}`)
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                setUser(data);
+                localStorage.setItem('rows', data[0].preferences.rows)
+                localStorage.setItem('columns', data[0].preferences.columns)
+                localStorage.setItem('actionPoints', data[0].preferences.actionPoints)
+                localStorage.setItem('statPoints', data[0].preferences.statPoints)
+            });
     }, [])
 
     return (
         <>
             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                 <h2>About Bot Battle</h2>
-                <p style={{width: '50%', textIndent: '5%'}}>Bot Battle is a simple multiplayer game where 2-4 players battle each other with robots with customized stats in a turn based fashion on a hex based grid. Each bot has health, damage, range, and movement which can be customized and a set 2 actions per turn. Challenge is 1v1, Free For All (FFA) is a 3 player game, and Mayhem is a 4 person free for all!</p>
+                <p style={{ width: '50%', textIndent: '5%' }}>Bot Battle is a simple multiplayer game where 2-4 players battle each other with robots with customized stats in a turn based fashion on a hex based grid. Each bot has health, damage, range, and movement which can be customized and a number of actions per turn. Challenge is 1v1, Free For All (FFA) is a 3 player game, and Mayhem is a 4 person free for all! Default settings are 7 rows, 5 columns, 5 stat upgrade points, and 2 actions per turn.</p>
+                <h2>Preferences</h2>
+                {edit ?
+                    <section style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                            <p>Rows: </p>
+                            <input style={{ height: '50%' }} value={('' + rows).charAt(0) === '0' ? rows : '0' + rows} onChange={(event) => { if (/[0-9]/.test(event.target.value)) { setRows(event.target.value) } }}></input>
+                        </div>
+                        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                            <p>Columns: </p>
+                            <input style={{ height: '50%' }} value={('' + columns).charAt(0) === '0' ? columns : '0' + columns} onChange={(event) => { if (/[0-9]/.test(event.target.value)) { setColumns(event.target.value) } }}></input>
+                        </div>
+                        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                            <p>Upgrade points: </p>
+                            <input style={{ height: '50%' }} value={('' + statPoints).charAt(0) === '0' ? statPoints : '0' + statPoints} onChange={(event) => { if (/[0-9]/.test(event.target.value)) { setStatPoints(event.target.value) } }}></input>
+                        </div>
+                        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                            <p>Actions per turn: </p>
+                            <input style={{ height: '50%' }} value={('' + actionPoints).charAt(0) === '0' ? actionPoints : '0' + actionPoints} onChange={(event) => { if (/[0-9]/.test(event.target.value)) { setActionPoints(event.target.value) } }}></input>
+                        </div>
+                        <button onClick={submit}>Confirm</button>
+                    </section>
+                    :
+                    <section>
+                        <p>Rows: {user.map((users) => users.preferences.rows)}</p>
+                        <p>Columns: {user.map((users) => users.preferences.columns)}</p>
+                        <p>Upgrade points: {user.map((users) => users.preferences.statPoints)}</p>
+                        <p>Actions per turn: {user.map((users) => users.preferences.actionPoints)}</p>
+                        <button onClick={toggle}>Edit Preferences</button>
+                    </section>
+                }
             </div>
         </>
     )
